a84519aaa98871d4873615a498d59ba2b5d724bd
MKalwa <moritz.kalwa@arcor.de>
2017-05-03 15:52:48 +0200

    Fertig

---
+++ members.php
@@ -0,0 +1,13 @@
+<?php
+	$pdo = new PDO('mysql:host=sql11.freemysqlhosting.net;dbname=sql11165022', 'sql11165022', 'Qh36tkyfr1');
+    echo("<li><h1>Mitglieder</h1></li>");
+	$sql = "SELECT rang, name FROM members";
+	foreach ($pdo->query($sql) as $row) {
+        echo("<li><p>");
+        echo("[");
+		echo($row['rang']);
+		echo("] ");
+		echo($row['name']);
+        echo("</p></li>");
+	}
+?>
