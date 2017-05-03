9509bc1e4a3b9e9042c973b25f6547bd6ff49187
MKalwa <moritz.kalwa@arcor.de>
2017-05-03 16:01:23 +0200

    Fertig
    Fixed forum link and added coming soon for member list.

--- members.php
+++ members.php
@@ -1,4 +1,6 @@
 <?php
+	echo("Coming soon.");
+	exit();
 	$pdo = new PDO('mysql:host=sql11.freemysqlhosting.net;dbname=sql11165022', 'sql11165022', 'Qh36tkyfr1');
     echo("<li><h1>Mitglieder</h1></li>");
 	$sql = "SELECT rang, name FROM members";
